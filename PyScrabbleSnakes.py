c64033715e918a5d98c5e49f55d1ea2bac712912
Justin L. Brown <justin.brown@cpchem.com>
2019-12-04 16:01:05 -0600

    Reverted back to the simple hash set instead of a dictionary, since it's still the more performant option.

--- PyScrabbleSnakes.py
+++ PyScrabbleSnakes.py
@@ -9,40 +9,34 @@ args = parser.parse_args()
 # - Dan Aykroyd, Evolution, 2001.
 def main():
     # Initialize a collection of 3+ letter words from a word list.
-    snakes = {}
+    snakes = set()
     with open(args.wordlist, "r") as file:
         for word in file:
-            if len(word.strip()) >= 3:
-                snakes[word] = None
-
-    # Check every snake for life.
-    for snake in list(snakes):
-        if snakes[snake] is None:
-            snakes[snake] = isSnakeAlive(snake, snakes)
+            if len(word.rstrip()) >= 3:
+                snakes.add(word.rstrip())
 
     # Put all the living snakes into a bag.
-    bag = list({snake:isAlive for snake,isAlive in snakes.items() if isAlive == True})
+    bag = []
+    for snake in snakes:
+        if isSnakeAlive(snake, snakes):
+            bag.append(snake)
 
-    # Sort the snakes in the bag by length and then alphabetically,
-    # and then lay them out straight in a text file.
+	# Sort the snakes in the bag by length and then alphabetically,
+	# and then lay them out straight in a text file.
     bag.sort(key=lambda snake: (-len(snake), snake))
     with open(args.scrabblesnakes, "w") as file:
-        file.writelines(bag)
+        file.writelines(snake + '\n' for snake in bag)
 
 def isSnakeAlive(snake, snakes):
-    # The snake must be a word to be alive.
-    if snake not in snakes.keys():
-        return False
-    # Check a snake for life at most once.
-    elif snakes[snake] is None:
-        # A snake is alive if it is three letters long.
-        if len(snake.strip()) == 3:
-            snakes[snake] = True
-        # A snake is alive if cutting off its tail leaves a living snake.
-        else:
-            snakes[snake] = isSnakeAlive(snake[1:], snakes)
-    # Return the result of checking the snake for life.
-    return snakes[snake]
+	# A living snake is itself a word, and
+		# it is three letters long, or
+		# if cutting off its tail (the left-most letter) leaves a living snake.
+	if snake not in snakes:
+		return False
+	elif len(snake) == 3:
+		return True
+	else:
+		return isSnakeAlive(snake[1:], snakes)
 
 if __name__ == "__main__":
     main()
